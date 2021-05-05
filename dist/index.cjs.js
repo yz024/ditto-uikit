f09fb2fd42342e2b8bf2a75d3406038cea780b95
Ang Yang Cheng <>
2021-05-05 23:48:01 +0800

    hi

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2139,14 +2139,14 @@ var socials = [
         items: [
             {
                 label: "Discussion",
-                href: "/",
+                href: "https://t.me/squirtlefinance/",
             },
         ],
     },
     {
         label: "Twitter",
         icon: "TwitterIcon",
-        href: "/",
+        href: "https://twitter.com/SquirtleFinance/",
     },
 ];
 var MENU_HEIGHT = 64;
