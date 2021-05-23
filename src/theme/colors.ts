92c1c3858e798746a9f4c4b441c6fd88e82db50e
Ang <ang@Angs-MacBook-Air.local>
2021-05-23 22:02:13 +0800

    next

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,10 +2,10 @@ import { Colors } from "./types";
 
 export const baseColors = {
   failure: "#ED4B9E",
-  primary: "#55A3AB",
-  primaryBright: "#55A3AB",
-  primaryDark: "#55A3AB",
-  secondary: "#55A3AB",
+  primary: "#aa8929",
+  primaryBright: "#aa8929",
+  primaryDark: "#aa8929",
+  secondary: "#7645D9",
   success: "#31D0AA",
   warning: "#FFB237",
 };
@@ -23,9 +23,9 @@ export const lightColors: Colors = {
   invertedContrast: "#FFFFFF",
   input: "#eeeaf4",
   tertiary: "#EFF4F5",
-  text: "#55A3AB",
+  text: "#452A7A",
   textDisabled: "#BDC2C4",
-  textSubtle: "#55A3AB",
+  textSubtle: "#8f80ba",
   borderColor: "#E9EAEB",
   card: "#FFFFFF",
   gradients: {
@@ -36,17 +36,17 @@ export const lightColors: Colors = {
 export const darkColors: Colors = {
   ...baseColors,
   ...brandColors,
-  secondary: "#55A3AB",
+  secondary: "#9A6AFF",
   background: "#343135",
   backgroundDisabled: "#3c3742",
   contrast: "#FFFFFF",
   invertedContrast: "#191326",
   input: "#483f5a",
-  primaryDark: "#55A3AB",
+  primaryDark: "#0098A1",
   tertiary: "#353547",
-  text: "#add8e6",
+  text: "#EAE2FC",
   textDisabled: "#666171",
-  textSubtle: "#add8e6",
+  textSubtle: "#c9c4d4",
   borderColor: "#524B63",
   card: "#27262c",
   gradients: {
